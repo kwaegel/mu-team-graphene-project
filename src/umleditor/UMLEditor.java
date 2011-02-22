2831c617feee1b95f01968668f886e5d4e253aa6
samgibbs11 <samgibbs11@gmail.com>
2011-02-22 16:15:25 -0500

    added get node

--- src/umleditor/UMLEditor.java
+++ src/umleditor/UMLEditor.java
@@ -2,23 +2,59 @@ package umleditor;
 
 
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JToolBar;
 
-public class UMLEditor extends JFrame {
+public class UMLEditor extends JFrame implements MouseListener {
 
 	ClassDiagram classDiagram;
 	JMenu menu;
 	JToolBar toolBar;
 	
-	public UMLEditor()
-	{
-		
-	}
-	
 	public static void main(String[] args) 
 	{
 		
+	}  
+	
+	public UMLEditor() {
+		
+		
+		
 	}
+
+	
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
 }
