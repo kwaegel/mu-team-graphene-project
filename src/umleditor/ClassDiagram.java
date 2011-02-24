b83743ac5328e048c58327ed5c787bbf4655c242
Ky Waegel <kwaegel@gmail.com>
2011-02-24 15:07:28 -0500

    Removed the test class.

--- src/umleditor/ClassDiagram.java
+++ src/umleditor/ClassDiagram.java
@@ -10,14 +10,16 @@ import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
 
-public class ClassDiagram implements MouseListener {
+public class ClassDiagram implements MouseListener
+{
 
 	private LinkedList<ClassNode> listofNodes;
 	private ClassNode selectedNode;
 	private UMLEditor parentEditor;
 	private JPanel view;
 
-	public ClassDiagram(UMLEditor parent) {
+	public ClassDiagram(UMLEditor parent)
+	{
 		parentEditor = parent;
 
 		view = new JPanel();
@@ -27,13 +29,15 @@ public class ClassDiagram implements MouseListener {
 
 		listofNodes = new LinkedList<ClassNode>();
 	}
-	
+
 	/**
-	 * Create a new node and add it to the list of nodes. Also add it's {@link NodePanel} to the view.
+	 * Create a new node and add it to the list of nodes. Also add it's
+	 * {@link NodePanel} to the view.
 	 * 
 	 * @return the newly created node.
 	 */
-	private void createNode(Point addLocation) {
+	private void createNode(Point addLocation)
+	{
 		ClassNode newClassNode = new ClassNode();
 		NodePanel newNodePanel = new NodePanel(this, newClassNode);
 
@@ -44,22 +48,26 @@ public class ClassDiagram implements MouseListener {
 		view.validate();
 	}
 
-	private void unselectCurrentNode() {
-		if (selectedNode != null) {
+	private void unselectCurrentNode()
+	{
+		if (selectedNode != null)
+		{
 			selectedNode.getNodePanel().makeUnselected();
 			selectedNode = null;
 		}
 		parentEditor.setDeleteButtonState(false);
 	}
 
-	public void setSelectedNode(ClassNode node) {
+	public void setSelectedNode(ClassNode node)
+	{
 		unselectCurrentNode();
 		selectedNode = node;
 		parentEditor.setDeleteButtonState(true);
 		parentEditor.disableAddNewClassMode();
 	}
 
-	public void deleteSelectedNode() {
+	public void deleteSelectedNode()
+	{
 		NodePanel panelToRemove = selectedNode.getNodePanel();
 		view.remove(panelToRemove);
 		view.paintImmediately(panelToRemove.getBounds());
@@ -68,40 +76,52 @@ public class ClassDiagram implements MouseListener {
 		parentEditor.setDeleteButtonState(false);
 	}
 
-	public void addRelationship(NodePanel secondNode) {
-		if (selectedNode != null) {
+	public void addRelationship(NodePanel secondNode)
+	{
+		if (selectedNode != null)
+		{
 			// add relationship
 		}
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
+	public void mouseClicked(MouseEvent arg0)
+	{
 		// mouse clicked in the view, not on any node
 		// check to see if adding a class is enabled
-		if (parentEditor.isAddNewClassModeEnabled()) {
+		if (parentEditor.isAddNewClassModeEnabled())
+		{
 			// add new class mode enabled, so add a new class
 			this.createNode(arg0.getPoint());
 			if (!arg0.isShiftDown())
+			{
 				parentEditor.disableAddNewClassMode();
-		} else {
+			}
+		}
+		else
+		{
 			this.unselectCurrentNode();
 		}
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
+	public void mouseEntered(MouseEvent arg0)
+	{
 	}
 
 	@Override
-	public void mouseExited(MouseEvent arg0) {
+	public void mouseExited(MouseEvent arg0)
+	{
 	}
 
 	@Override
-	public void mousePressed(MouseEvent arg0) {
+	public void mousePressed(MouseEvent arg0)
+	{
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent arg0) {
+	public void mouseReleased(MouseEvent arg0)
+	{
 	}
 
 }
