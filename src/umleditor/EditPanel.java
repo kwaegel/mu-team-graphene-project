2bdf912cf4ae41e590f5d397fa352abe752e1088
lmcrous1 <lmcrous1@localhost>
2011-02-28 11:36:22 -0500

    fixed errors introduced by merging

--- src/umleditor/EditPanel.java
+++ src/umleditor/EditPanel.java
@@ -2,6 +2,7 @@ package umleditor;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
