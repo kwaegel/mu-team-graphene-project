674af7ad2d63abd3b01ee13b61f365312e486630
Ky Waegel <kwaegel@gmail.com>
2011-03-01 14:39:01 -0500

    Added tag Submission #1 for changeset 492cc711d608

---
+++ src/umleditor/UMLEditor.java
@@ -0,0 +1,208 @@
+package umleditor;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+
+public class UMLEditor extends JFrame implements ActionListener
+{
+
+	private static final long serialVersionUID = -9139566399320553797L;
+	private Color unselectedButtonColor = javax.swing.UIManager.getColor("Button.background");
+	private Color selectedButtonColor = Color.gray;
+
+	private JMenuBar menuBar;
+	private JToolBar toolBar;
+
+	private JButton deleteButton;
+	private JButton addClassButton;
+	
+	private JScrollPane scrollPane;
+
+	private ClassDiagram classDiagram;
+
+	private boolean addNewClassModeEnabled;
+
+	public UMLEditor()
+	{
+		super("UML Editor");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setPreferredSize(new Dimension(800, 800));
+		this.setMinimumSize(new Dimension(250, 200));
+		this.setLocationByPlatform(true);
+
+		setUpMenuBar();
+		setUpToolBar();
+		setUpScrollPane();
+		setUpClassDiagram();
+
+		this.pack();
+		this.setVisible(true);
+	}
+
+	public boolean isAddNewClassModeEnabled()
+	{
+		return (addNewClassModeEnabled);
+	}
+
+	public void disableAddNewClassMode()
+	{
+		addNewClassModeEnabled = false;
+		addClassButton.setBackground(unselectedButtonColor);
+	}
+
+	public void setDeleteButtonState(boolean enabled)
+	{
+		deleteButton.setEnabled(enabled);
+	}
+
+	private void setUpMenuBar()
+	{
+		menuBar = new JMenuBar();
+
+		JMenu fileMenu = new JMenu("File");
+
+		JMenuItem newOption = new JMenuItem("New");
+		newOption.setActionCommand("NEW");
+		newOption.addActionListener(this);
+		fileMenu.add(newOption);
+
+		// JMenuItem loadOption = new JMenuItem("Load...");
+		// fileMenu.add(loadOption);
+		//
+		// JMenuItem saveOption = new JMenuItem("Save");
+		// fileMenu.add(saveOption);
+		//
+		// fileMenu.addSeparator();
+		//
+		// JMenuItem printOption = new JMenuItem("Print...");
+		// fileMenu.add(printOption);
+
+		fileMenu.addSeparator();
+
+		JMenuItem exitOption = new JMenuItem("Exit");
+		exitOption.setActionCommand("EXIT");
+		exitOption.addActionListener(this);
+		fileMenu.add(exitOption);
+
+		menuBar.add(fileMenu);
+
+		// JMenu helpMenu = new JMenu("Help");
+		// menuBar.add(helpMenu);
+
+		this.add(menuBar, BorderLayout.NORTH);
+
+	}
+
+	private void setUpToolBar()
+	{
+		toolBar = new JToolBar();
+
+		addClassButton = new JButton("Add Class");
+		addClassButton.setActionCommand("ADD");
+		addClassButton.addActionListener(this);
+		addClassButton.setFocusPainted(false);
+		toolBar.add(addClassButton);
+		addNewClassModeEnabled = false;
+
+		deleteButton = new JButton("Delete");
+		deleteButton.setActionCommand("DELETE");
+		deleteButton.addActionListener(this);
+		deleteButton.setEnabled(false);
+		toolBar.add(deleteButton);
+
+		this.add(toolBar, BorderLayout.SOUTH);
+	}
+	
+	private void setUpScrollPane()
+	{
+		scrollPane = new JScrollPane();
+	}
+
+	private void setUpClassDiagram()
+	{
+		classDiagram = new ClassDiagram(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+		if (arg0.getActionCommand() == "ADD")
+		{
+			// in most cases, we'll get this action event because we're enabling
+			// Add-Class mode, so set background appropriately
+			addClassButton.setBackground(selectedButtonColor);
+			if (addNewClassModeEnabled)
+			{
+				// if Add-Class mode was already selected,
+				// this click unselects it, so reset background
+				// to normal color
+				addClassButton.setBackground(unselectedButtonColor);
+			}
+			// toggle Add-Class state
+			addNewClassModeEnabled = !addNewClassModeEnabled;
+		}
+		else if (arg0.getActionCommand() == "DELETE")
+		{
+			classDiagram.deleteSelectedNode();
+
+		}
+		else if (arg0.getActionCommand() == "NEW")
+		{
+			clearDiagram();
+		}
+		else if (arg0.getActionCommand() == "EXIT")
+		{
+			// When save is implemented, message will be changed to: Do you want
+			// to save?
+			JFrame frame = new JFrame();
+			String message = "Are you sure you want to quit?";
+			int answer = JOptionPane.showConfirmDialog(frame, message);
+			if (answer == JOptionPane.YES_OPTION)
+			{
+				System.exit(0);
+			}
+			else if (answer == JOptionPane.NO_OPTION)
+			{
+
+			}
+
+		}
+	}
+
+	/**
+	 * Used when New is selected in the File menu. Deletes everything in the diagram
+	 */
+	public void clearDiagram()
+	{
+		this.disableAddNewClassMode();
+		classDiagram = new ClassDiagram(this);
+		this.validate();
+	}
+	
+	public JScrollPane getScrollPane()
+	{
+		return(scrollPane);
+	}
+
+	/**
+	 * Entry point for the program.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		new UMLEditor();
+	}
+}
---
+++ src/unitTesting/JUnitTests.java
@@ -0,0 +1,105 @@
+package unitTesting;
+
+import org.junit.Test;
+
+import umleditor.ClassDiagram;
+import umleditor.ClassNode;
+import umleditor.NodePanel;
+import umleditor.UMLEditor;
+import static org.junit.Assert.*;
+
+public class JUnitTests extends UMLEditor
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3943944413362019489L;
+
+	/**
+	 * Creates a testnode and tests the default settings (name, attributes, methods)
+	 */
+	@Test
+	public void testConstructors()
+	{
+		ClassNode testNode = new ClassNode();
+		assertTrue("Error: not constructed correctly, wrong number of attributes", testNode.getNumAttributes() == 1);
+		assertTrue("Error: not constructed correctly, wrong number of methods", testNode.getNumMethods() == 1);
+
+	}
+
+	/*
+	 * create a ClassDiagram,ClassNode, and NodePanel. Then delete new node
+	 */
+	@Test
+	public void testDelete()
+	{
+		ClassDiagram testDiagram = new ClassDiagram(this);
+		ClassNode testNode = new ClassNode();
+		@SuppressWarnings("unused")
+		NodePanel testNodePanel = new NodePanel(testDiagram, testNode);
+
+		testDiagram.setSelectedNode(testNode);
+		testDiagram.deleteSelectedNode();
+
+	}
+
+	/*
+	 * create several methods, add them, remove them
+	 */
+	@Test
+	public void testMethods()
+	{
+
+		ClassNode testNode = new ClassNode(); // Method 1 is automatically created on Node creation
+		testNode.attachPanel(new NodePanel(new ClassDiagram(new UMLEditor()), testNode));
+		testNode.addMethod("Method 2");
+		testNode.addMethod("Method 3");
+		testNode.addMethod("Method 4");
+		testNode.addMethod("Method 5");
+		assertTrue("Error: incorrect number of methods in class", testNode.getNumMethods() == 5);
+
+		// remove 3 methods
+		testNode.removeMethod(4);
+		testNode.removeMethod(3);
+		testNode.removeMethod(2);
+		assertTrue("Error: Methods were not all removed", testNode.getNumMethods() < 3);
+		assertTrue("Error: more Methods removed than should have been", testNode.getNumMethods() > 1);
+
+	}
+	
+	/*
+	 * create several Attributes, add them, remove them
+	 */
+	@Test
+	public void testAttributes()
+	{
+
+		ClassNode testNode = new ClassNode(); // Attribute 1 is automatically created on Node creation
+		testNode.attachPanel(new NodePanel(new ClassDiagram(new UMLEditor()), testNode));
+		testNode.addAttribute("Attrib 2");
+		testNode.addAttribute("Attrib 3");
+		testNode.addAttribute("Attrib 4");
+		testNode.addAttribute("Attrib 5");
+		assertTrue("Error: incorrect number of Attributes in class", testNode.getNumAttributes() == 5);
+
+		// remove 3 Attributes
+		testNode.removeAttribute(4);
+		testNode.removeAttribute(3);
+		testNode.removeAttribute(2);
+		assertTrue("Error: Attributes were not all removed", testNode.getNumAttributes() < 3);
+		assertTrue("Error: more Attributes removed than should have been", testNode.getNumAttributes() > 1);
+
+	}
+	
+	@Test
+	public void testNaming()
+	{
+		ClassNode testNode = new ClassNode();
+		testNode.attachPanel(new NodePanel(new ClassDiagram(new UMLEditor()), testNode));
+		testNode.setName("ThisIsTheTestNode");
+		String name = testNode.getName();
+		assertTrue("Error: Class name was not set, or get does not work", name.equals("ThisIsTheTestNode"));
+	}
+
+}
