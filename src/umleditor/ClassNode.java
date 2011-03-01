821b125d22a460a51b1aec8bd080a3576774995f
Ky Waegel <kwaegel@gmail.com>
2011-03-01 12:04:15 -0500

    Removed class dependency.
    Relationship now asks the ClassNode for its bounds insted of asking for the panel first.

--- src/umleditor/ClassNode.java
+++ src/umleditor/ClassNode.java
@@ -1,5 +1,6 @@
 package umleditor;
 
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,6 +44,14 @@ public class ClassNode
 	public String getName()
 	{
 		return className;
+	}
+
+	/**
+	 * @return - the bounds of this node.
+	 */
+	public Rectangle getBounds()
+	{
+		return nodePanel.getBounds();
 	}
 
 	// Sets the name of the Class Node
