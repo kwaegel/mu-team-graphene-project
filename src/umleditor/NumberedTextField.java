9fe13d39ca0c79399237ad9f6b5104d977fb3ca3
lmcrous1 <lmcrous1@localhost>
2011-03-15 15:21:11 -0400

    made some minor changes to names, layout, edit panel

---
+++ src/umleditor/NumberedTextField.java
@@ -0,0 +1,33 @@
+package umleditor;
+
+import javax.swing.JTextField;
+
+public class NumberedTextField extends JTextField
+{
+	public enum FieldType {Attribute, Method, ClassName}
+	private static final long serialVersionUID = 3673040587471673525L;
+	private int numberIndex;
+	private FieldType myType;
+
+	public NumberedTextField(String initialContents, int number, FieldType type)
+	{
+		super(initialContents);
+		numberIndex = number;
+		myType = type;
+	}
+	
+	public int getNumberIndex()
+	{
+		return (numberIndex);
+	}
+	
+	public void setNumberIndex(int number)
+	{
+		numberIndex = number;
+	}
+	
+	public FieldType getType()
+	{
+		return (myType);
+	}
+}
