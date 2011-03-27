88da5ec6b66fbffd2c0fc36ecdfeb99a29c040ba
lmcrous1 <lmcrous1@localhost>
2011-03-27 17:03:26 -0400

    ran ctrl-shift-o

--- src/umleditor/UMLEditor.java
+++ src/umleditor/UMLEditor.java
@@ -6,7 +6,6 @@ import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
