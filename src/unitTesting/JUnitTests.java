1bb69b993e2d4bd9c4854ec3026d838c043c5c26
lmcrous1 <lmcrous1@localhost>
2011-03-29 10:37:46 -0400

    junit

--- src/unitTesting/JUnitTests.java
+++ src/unitTesting/JUnitTests.java
@@ -1,6 +1,7 @@
 package unitTesting;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import javax.swing.JScrollPane;
 
@@ -13,7 +14,7 @@ import umleditor.NumberedTextField;
 import umleditor.NumberedTextField.FieldType;
 import umleditor.UMLEditor;
 
-public class JUnitTests extends UMLEditor
+public class JUnitTests /*extends UMLEditor*/
 {
 
 	/**
@@ -29,7 +30,9 @@ public class JUnitTests extends UMLEditor
 	{
 		ClassNode testNode = new ClassNode();
 		assertTrue("Error: not constructed correctly, wrong number of attributes", testNode.getNumAttributes() == 1);
+		assertTrue("Error: first attribute has wrong name", testNode.getAttribute(0).equals("attribute 1"));
 		assertTrue("Error: not constructed correctly, wrong number of methods", testNode.getNumMethods() == 1);
+		assertTrue("Error: first method has wrong name", testNode.getMethod(0).equals("method 1"));
 
 	}
 
@@ -39,7 +42,7 @@ public class JUnitTests extends UMLEditor
 	@Test
 	public void testDelete()
 	{
-		ClassDiagram testDiagram = new ClassDiagram(this, new JScrollPane());
+		ClassDiagram testDiagram = new ClassDiagram(new UMLEditor(), new JScrollPane());
 		ClassNode testNode = new ClassNode();
 		testNode.attachPanel(new NodePanel(testDiagram, testNode));
 
@@ -56,22 +59,42 @@ public class JUnitTests extends UMLEditor
 	@Test
 	public void testMethods()
 	{
-
+		String m1 = "this is the second method name";
+		String m2 = "Method 3";
+		String m3 = "Method 4";
+		String m4 = "Method 5";
 		ClassNode testNode = new ClassNode(); // Method 1 is automatically created on Node creation
 		testNode.attachPanel(new NodePanel(new ClassDiagram(new UMLEditor(), new JScrollPane()), testNode));
-		testNode.addMethod("Method 2");
-		testNode.addMethod("Method 3");
-		testNode.addMethod("Method 4");
-		testNode.addMethod("Method 5");
+		testNode.addMethod(m1);
+		assertTrue("Error: incorrect number of methods in class", testNode.getNumMethods() == 2);
+		assertTrue("Error: second method has wrong name", testNode.getMethod(1).equals(m1));
+		testNode.addMethod(m2);
+		assertTrue("Error: incorrect number of methods in class", testNode.getNumMethods() == 3);
+		assertTrue("Error: third method has wrong name", testNode.getMethod(2).equals(m2));
+		testNode.addMethod(m3);
+		assertTrue("Error: incorrect number of methods in class", testNode.getNumMethods() == 4);
+		assertTrue("Error: fourth method has wrong name", testNode.getMethod(3).equals(m3));
+		testNode.addMethod(m4);
 		assertTrue("Error: incorrect number of methods in class", testNode.getNumMethods() == 5);
+		assertTrue("Error: fourth method has wrong name", testNode.getMethod(4).equals(m4));
 
-		// remove 3 methods
 		testNode.removeMethod(4);
-		testNode.removeMethod(3);
+		assertTrue("Error: incorrect number of methods in class", testNode.getNumMethods() == 4);
+		assertTrue("Error: first method has wrong name", testNode.getMethod(0).equals("method 1"));
+		assertTrue("Error: second method has wrong name", testNode.getMethod(1).equals(m1));
+		assertTrue("Error: third method has wrong name", testNode.getMethod(2).equals(m2));
+		
 		testNode.removeMethod(2);
+		assertTrue("Error: incorrect number of methods in class", testNode.getNumMethods() == 3);
+		assertTrue("Error: first method has wrong name", testNode.getMethod(0).equals("method 1"));
+		assertTrue("Error: second method has wrong name", testNode.getMethod(1).equals(m1));
+		assertTrue("Error: third method has wrong name", testNode.getMethod(2).equals(m3));
+		
+		testNode.removeMethod(0);
 		assertTrue("Error: Methods were not all removed", testNode.getNumMethods() < 3);
 		assertTrue("Error: more Methods removed than should have been", testNode.getNumMethods() > 1);
-
+		assertTrue("Error: first method has wrong name", testNode.getMethod(0).equals(m1));
+		assertTrue("Error: second method has wrong name", testNode.getMethod(1).equals(m3));
 	}
 	
 	/*
