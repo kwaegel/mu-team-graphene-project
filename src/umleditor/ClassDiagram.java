e34dadfd3089fbac02545f4eb022830d0fa5c029
MaxMSeibert <maxmseibert@gmail.com>
2011-03-31 16:29:45 -0400

    Fixed JUnit problem

---
+++ src/umleditor/ClassDiagram.java
@@ -0,0 +1,508 @@
+package umleditor;
+
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+
+import net.miginfocom.swing.MigLayout;
+import umleditor.Relationship.RelationshipType;
+
+import com.thoughtworks.xstream.XStream;
+
+/**
+ * A ClassDiagram contains all information associated with a UML diagram in the {@link UMLEditor}. It maintains the
+ * models for Relationships and Classes in the diagram. It is responsible for creating nodes and connecting them with
+ * relationships. It contains a reference to the view in which the classes and relationships are displayed. It keeps
+ * track of the currently selected object in the diagram deletes it when appropriate. It manages cutting and copying
+ * classes from, and pasting classes to itself. It knows what file it was saved to, and keeps track of whether or not it
+ * was saved.
+ */
+public class ClassDiagram implements KeyListener, FocusListener
+{
+	// Lists of objects in the diagram
+	private List<ClassNode> listOfNodes;
+	private List<RelationshipModel> m_relationships;
+
+	// Listeners for mouse events on the diagram
+	private transient RelationshipDragListener m_relationshipDragController;
+
+	private transient ISelectable currentlySelectedObject;
+
+	private transient UMLEditor parentEditor;
+	private transient JLayeredPane view;
+
+	private transient File fileSavedTo;
+	private transient boolean changedSinceSaved;
+
+	public ClassDiagram(UMLEditor parent, JScrollPane scrollPane)
+	{
+		parentEditor = parent;
+
+		view = new JLayeredPane();
+		// view.addMouseListener(this);
+		view.setFocusable(true);
+		view.addKeyListener(this);
+		view.setLayout(new MigLayout("", "", ""));
+		view.addFocusListener(this);
+
+		// Add the view to the scroll pane.
+		scrollPane.setViewportView(view);
+
+		changedSinceSaved = true;
+
+		listOfNodes = new LinkedList<ClassNode>();
+		m_relationships = new LinkedList<RelationshipModel>();
+
+		// Create listeners on the view.
+		m_relationshipDragController = new RelationshipDragListener(this);
+
+		view.addMouseListener(new MouseClickListener());
+	}
+
+	/**
+	 * Ensures the view gets focus when a new ClassDiagram has been created. Called whenever a new ClassDiagram is
+	 * created in the UML Editor.
+	 */
+	public void requestFocusOnView()
+	{
+		view.requestFocus();
+	}
+
+	/**
+	 * Create a new node and initializes it.
+	 */
+	private void createNode(Point addLocation)
+	{
+		ClassNode newClassNode = new ClassNode();
+		initNode(addLocation, newClassNode);
+		this.markAsChanged();
+	}
+
+	/**
+	 * Adds new node to the list of nodes. Also add it's {@link NodePanel} to the view.
+	 * 
+	 * @param addLocation
+	 *            - location to add node
+	 * @param newClassNode
+	 *            - node to add
+	 */
+	private void initNode(Point addLocation, ClassNode newClassNode)
+	{
+		NodePanel newNodePanel = new NodePanel(this, newClassNode);
+		newNodePanel.attachToView(view);
+		newNodePanel.resetBounds(addLocation);
+
+		listOfNodes.add(newClassNode);
+
+		this.setSelectedObject(newClassNode);
+		view.revalidate();
+	}
+
+	/**
+	 * Deselect the currently selected object and disable the delete button.
+	 */
+	private void unselectCurrentObject()
+	{
+		if (currentlySelectedObject != null)
+		{
+			currentlySelectedObject.setSelected(false);
+			currentlySelectedObject = null;
+			parentEditor.setDeleteButtonState(false);
+		}
+	}
+
+	/**
+	 * Notify the classDiagram of object selection. Sending null indicates that no object was selected.
+	 * 
+	 * @param selected
+	 */
+	public void setSelectedObject(ISelectable selected)
+	{
+		if (selected != currentlySelectedObject)
+		{
+			unselectCurrentObject();
+			selected.setSelected(true);
+		}
+		currentlySelectedObject = selected;
+
+		// Turn the delete button on if something non-null was selected.
+		parentEditor.setDeleteButtonState(selected != null ? true : false);
+
+		parentEditor.disableAddNewClassMode();
+	}
+
+	/**
+	 * Deletes the selected object. The selected object can either be a ClassNode or Relationship.
+	 */
+	public void deleteSelectedObject()
+	{
+		if (currentlySelectedObject instanceof ClassNode)
+		{
+			ClassNode node = (ClassNode) currentlySelectedObject;
+			removeRelationships(node.getRelationships());
+
+			// Remove the view part
+			NodePanel panelToRemove = node.getNodePanel();
+			view.remove(panelToRemove);
+
+			// need this call so deleting nodes not at edges of screen works properly
+			view.repaint();
+
+			// need this call so deleting nodes at edges of screen works properly
+			view.revalidate();
+
+			listOfNodes.remove(node);
+		}
+		else if (currentlySelectedObject instanceof Relationship)
+		{
+			Relationship r = (Relationship) currentlySelectedObject;
+			if (r.isControlPointSelected())
+			{
+				r.removeSelectedControlPoint();
+			}
+			else
+			{
+				// Remove the entire relationship.
+				r.removeFromLinkedNodes();
+				m_relationships.remove(r);
+				view.remove(r);
+			}
+			view.repaint(r.getBounds());
+		}
+
+		currentlySelectedObject = null;
+
+		parentEditor.setDeleteButtonState(false);
+		this.markAsChanged();
+	}
+
+	/**
+	 * Workaround for bug with improper MouseReleased event handling in Swing.
+	 * 
+	 * @param evt
+	 *            - the mouse event
+	 * @return the component under this event
+	 */
+	public Component getComponentUnder(MouseEvent evt)
+	{
+		Point p = ((Component) evt.getSource()).getLocation();
+		evt.translatePoint((int) p.getX(), (int) p.getY());
+		return view.getComponentAt(evt.getX(), evt.getY());
+	}
+
+	/**
+	 * Adds the relationship to the ClassDiagram's currently selected object, if that object is a ClassNode
+	 * 
+	 * @param secondNode
+	 *            - the node that relationship will end on.
+	 */
+	public void addRelationship(ClassNode secondNode)
+	{
+		if (currentlySelectedObject instanceof ClassNode)
+		{
+			addRelationship((ClassNode) currentlySelectedObject, secondNode);
+		}
+	}
+
+	/**
+	 * Adds a relationship between the two nodes. Private internal method in ClassDiagram.
+	 * 
+	 * @param firstNode
+	 *            - the starting node
+	 * @param secondNode
+	 *            - the ending node
+	 */
+	private void addRelationship(ClassNode firstNode, ClassNode secondNode)
+	{
+		// Do not add relationships between identical classes.
+		if (firstNode != secondNode)
+		{
+			RelationshipType[] possibleValues = RelationshipType.values();
+
+			int selection = JOptionPane.showOptionDialog(parentEditor, "Choose a type of relationship",
+					"Relationship Chooser", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null,
+					possibleValues, RelationshipType.Aggregation);
+
+			if (selection >= 0)
+			{
+				RelationshipType selectedType = possibleValues[selection];
+
+				Relationship rel = new Relationship(firstNode, secondNode, selectedType);
+
+				firstNode.addRelationship(rel);
+				secondNode.addRelationship(rel);
+
+				// Add the relationship to the model list
+				m_relationships.add(rel.getModel());
+
+				// Add the relationship to the view.
+				// Using the "external" constraint prevents MigLayout from changing the bounds of the relationship.
+				view.add(rel, "external");
+				rel.addMouseListener(m_relationshipDragController);
+				rel.addMouseMotionListener(m_relationshipDragController);
+
+				rel.repaint();
+			}
+
+			this.markAsChanged();
+		}
+	}
+
+	/**
+	 * Removes a list of relationships from the diagram. Called from a ClassNode whenever it gets deleted.
+	 * 
+	 * @param relationshipList
+	 */
+	public void removeRelationships(List<RelationshipModel> relationships)
+	{
+		// Clone list to prevent concurrent modification of the same list
+		List<RelationshipModel> relationshipList = new ArrayList<RelationshipModel>(relationships);
+
+		m_relationships.removeAll(relationshipList);
+		for (RelationshipModel rm : relationshipList)
+		{
+			// Remove the model
+			rm.removeFromLinkedNodes();
+
+			// Remove the view.
+			Relationship r = rm.getRelationship();
+			r.removeMouseListener(m_relationshipDragController);
+			view.remove(r);
+
+		}
+	}
+
+	/**
+	 * Saves this {@link ClassDiagram} to a file. If chooseNewFile is <code>true</code>, or no file name has been
+	 * associated with this diagram, will display a JFileChoser to get user to select a file.
+	 * 
+	 * @param chooseNewFile
+	 *            - whether or not to find a new file before saving.
+	 */
+	public void saveToFile(boolean chooseNewFile)
+	{
+		if (fileSavedTo == null || chooseNewFile)
+		{
+			JFileChooser fileSaveChooser = new JFileChooser();
+			fileSaveChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			fileSaveChooser.setMultiSelectionEnabled(false);
+			fileSaveChooser.addChoosableFileFilter(new FileExtensionFilter());
+			int userChoice = fileSaveChooser.showSaveDialog(parentEditor);
+			if (userChoice == JFileChooser.APPROVE_OPTION)
+			{
+				fileSavedTo = fileSaveChooser.getSelectedFile();
+			}
+		}
+		if (fileSavedTo != null && (changedSinceSaved || chooseNewFile))
+		{
+			FileWriter fileOutStream;
+			BufferedWriter buffOutStream;
+			try
+			{
+				// code to save to file goes here
+				XStream xmlStream = new XStream();
+
+				fileOutStream = new FileWriter(fileSavedTo);
+				buffOutStream = new BufferedWriter(fileOutStream);
+
+				xmlStream.toXML(this, buffOutStream);
+
+				buffOutStream.close();
+			}
+			catch (IOException e)
+			{
+
+			}
+
+			changedSinceSaved = false;
+			this.setTabTitle(fileSavedTo.getName());
+		}
+	}
+
+	public void markAsChanged()
+	{
+		if (fileSavedTo != null)
+		{
+			changedSinceSaved = true;
+			this.setTabTitle(fileSavedTo.getName() + "*");
+		}
+	}
+
+	public boolean isUnsaved()
+	{
+		boolean diagramBlank = (fileSavedTo == null && listOfNodes.isEmpty());
+		return (!diagramBlank && changedSinceSaved);
+	}
+
+	private void setTabTitle(String title)
+	{
+		JTabbedPane containingTabbedPane = (JTabbedPane) (view.getParent().getParent().getParent());
+		TabComponent tabComponent = (TabComponent) containingTabbedPane.getTabComponentAt(containingTabbedPane
+				.getSelectedIndex());
+		tabComponent.setTitle(title);
+		// containingTabbedPane.setTitleAt(containingTabbedPane.getSelectedIndex(), title);
+	}
+
+	public void copyNode()
+	{
+		if (currentlySelectedObject instanceof ClassNode)
+		{
+			parentEditor.setCopyNode((ClassNode) currentlySelectedObject);
+		}
+	}
+
+	public void cutNode()
+	{
+		if (currentlySelectedObject instanceof ClassNode)
+		{
+			parentEditor.setCopyNode((ClassNode) currentlySelectedObject);
+			this.deleteSelectedObject();
+		}
+	}
+
+	public void pasteNode()
+	{
+		ClassNode copy = parentEditor.getCopyNode();
+		if (copy != null)
+		{
+			Point pastePosition;
+			if (view.hasFocus())
+			{
+				pastePosition = view.getMousePosition();
+			}
+			else
+			{
+				pastePosition = new Point((parentEditor.getWidth() - 100) / 2, (parentEditor.getHeight() - 140) / 2);
+			}
+			System.out.println(pastePosition);
+			ClassNode nodeCopy = new ClassNode(copy);
+			initNode(pastePosition, nodeCopy);
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event)
+	{
+		if (event.getKeyCode() == KeyEvent.VK_DELETE && currentlySelectedObject != null)
+		{
+			this.deleteSelectedObject();
+		}
+		else if (event.getKeyCode() == KeyEvent.VK_N)
+		{
+			parentEditor.enableAddNewClassMode();
+		}
+		else if (event.getKeyCode() == KeyEvent.VK_V && event.isControlDown())
+		{
+			// Point mouseLocation = arg0.getComponent().getMousePosition();
+			// pasteNode(mouseLocation);
+		}
+		else if (event.getKeyCode() == KeyEvent.VK_E && currentlySelectedObject instanceof ClassNode)
+		{
+			ClassNode node = (ClassNode) currentlySelectedObject;
+			node.getNodePanel().displayEditPanel();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0)
+	{
+		// do nothing
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0)
+	{
+		// do nothing
+	}
+
+	public void movePanel(NodePanel nodePanelToMove, Point movePoint)
+	{
+		this.setSelectedObject(nodePanelToMove.getClassNode());
+
+		// view.remove(nodePanelToMove);
+
+		int newPosX = Math.max(nodePanelToMove.getX() + movePoint.x, 0);
+		int newPosY = Math.max(nodePanelToMove.getY() + movePoint.y, 0);
+		nodePanelToMove.resetBounds(new Point(newPosX, newPosY));
+
+		// call to revalidate makes node redraw.
+		view.revalidate();
+
+		// call to repaint makes relationships redraw
+		view.repaint();
+
+		this.markAsChanged();
+	}
+
+	/**
+	 * When a diagram becomes visible in the UML editor, ensure that the editor's delete button appropriately reflects
+	 * the current diagram.
+	 * 
+	 * @param e
+	 */
+	@Override
+	public void focusGained(FocusEvent e)
+	{
+		if (currentlySelectedObject != null)
+		{
+			parentEditor.setDeleteButtonState(true);
+		}
+		else
+		{
+			parentEditor.setDeleteButtonState(false);
+		}
+	}
+
+	@Override
+	public void focusLost(FocusEvent e)
+	{
+		// do nothing
+	}
+
+	/**
+	 * This class listens for clicks to an empty part of the class diagram and creates a new ClassNode if the new node
+	 * button is enabled.
+	 */
+	private class MouseClickListener extends MouseAdapter
+	{
+
+		@Override
+		public void mouseReleased(MouseEvent arg0)
+		{
+			// mouse clicked in the view, not on any node
+			// check to see if adding a class is enabled
+			if (parentEditor.isAddNewClassModeEnabled())
+			{
+				// add new class mode enabled, so add a new class
+				createNode(arg0.getPoint());
+				if (arg0.isShiftDown())
+				{
+					parentEditor.enableAddNewClassMode();
+				}
+			}
+			else
+			{
+				unselectCurrentObject();
+			}
+		}
+	}
+
+}
---
+++ src/umleditor/UMLEditor.java
@@ -0,0 +1,486 @@
+package umleditor;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
+
+import com.thoughtworks.xstream.XStream;
+
+public class UMLEditor extends JFrame implements ActionListener
+{
+	private static final long serialVersionUID = -9139566399320553797L;
+
+	private Color unselectedButtonColor = javax.swing.UIManager.getColor("Button.background");
+	private Color selectedButtonColor = Color.gray;
+
+	private HelpPanel helpPanel;
+
+	private JMenuBar menuBar;
+	private JToolBar toolBar;
+
+	private JButton deleteButton;
+	private JButton addClassButton;
+
+	private JTabbedPane tabbedPane;
+
+	private List<ClassDiagram> classDiagrams;
+
+	private ClassNode copyNode;
+
+	private boolean addNewClassModeEnabled;
+
+	public UMLEditor()
+	{
+		super("UML Editor");
+		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		this.setPreferredSize(new Dimension(800, 800));
+		this.setMinimumSize(new Dimension(250, 200));
+		this.setLocationByPlatform(true);
+		this.addWindowListener(new WindowCloseListener());
+
+		classDiagrams = new ArrayList<ClassDiagram>();
+
+		setUpMenuBar();
+		setUpToolBar();
+		setUpTabbedPane();
+		createNewClassDiagram();
+		setUpHelpPanel();
+
+		this.pack();
+		this.setVisible(true);
+	}
+
+	/**
+	 * Returns whether or not Add-Class mode is enabled. ClassDiagram checks this on mouse released and if it is
+	 * enabled, adds a new node to the diagram.
+	 * 
+	 * @return - <code>true</code> if Add-Class mode is enabled, <code>false</code> if it is not.
+	 */
+	public boolean isAddNewClassModeEnabled()
+	{
+		return (addNewClassModeEnabled);
+	}
+
+	/**
+	 * Enables Add-Class mode. Changes "Add Class" button color to indicate that it is selected and sets cursor.
+	 */
+	public void enableAddNewClassMode()
+	{
+		addNewClassModeEnabled = true;
+		addClassButton.setBackground(selectedButtonColor);
+		this.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+	}
+
+	/**
+	 * Disables Add-Class mode. Changes button background back to normal color and restores default cursor.
+	 */
+	public void disableAddNewClassMode()
+	{
+		addNewClassModeEnabled = false;
+		addClassButton.setBackground(unselectedButtonColor);
+		setCursor(Cursor.getDefaultCursor());
+	}
+
+	/**
+	 * Enables Delete Button. The delete button becomes clickable.
+	 */
+	public void setDeleteButtonState(boolean enabled)
+	{
+		deleteButton.setEnabled(enabled);
+	}
+
+	/**
+	 * Sets up the File and Help menu bars File menu contains New, Load, Close, Save, Save As, and Exit
+	 */
+	private void setUpMenuBar()
+	{
+		menuBar = new JMenuBar();
+
+		JMenu fileMenu = new JMenu("File");
+
+		JMenuItem newOption = new JMenuItem("New");
+		newOption.setActionCommand("NEW");
+		newOption.addActionListener(this);
+		newOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+		fileMenu.add(newOption);
+
+		JMenuItem loadOption = new JMenuItem("Load...");
+		loadOption.setActionCommand("LOAD");
+		loadOption.addActionListener(this);
+		loadOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
+		fileMenu.add(loadOption);
+
+		JMenuItem closeOption = new JMenuItem("Close");
+		closeOption.setActionCommand("CLOSE");
+		closeOption.addActionListener(this);
+		closeOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_DOWN_MASK));
+		fileMenu.add(closeOption);
+
+		fileMenu.addSeparator();
+
+		JMenuItem saveOption = new JMenuItem("Save");
+		saveOption.setActionCommand("SAVE");
+		saveOption.addActionListener(this);
+		saveOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+		fileMenu.add(saveOption);
+
+		JMenuItem saveAsOption = new JMenuItem("Save As...");
+		saveAsOption.setActionCommand("SAVEAS");
+		saveAsOption.addActionListener(this);
+		fileMenu.add(saveAsOption);
+		//
+		// fileMenu.addSeparator();
+		//
+		// JMenuItem printOption = new JMenuItem("Print...");
+		// fileMenu.add(printOption);
+
+		fileMenu.addSeparator();
+
+		JMenuItem exitOption = new JMenuItem("Exit");
+		exitOption.setActionCommand("EXIT");
+		exitOption.addActionListener(this);
+		exitOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0));
+		fileMenu.add(exitOption);
+
+		menuBar.add(fileMenu);
+
+		JMenu editMenu = new JMenu("Edit");
+
+		/*
+		 * JMenuItem pasteOption = new JMenuItem("Paste"); pasteOption.setActionCommand("PASTE");
+		 * pasteOption.addActionListener(this); pasteOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V,
+		 * InputEvent.CTRL_DOWN_MASK)); editMenu.add(pasteOption);
+		 */
+
+		JMenuItem cutOption = new JMenuItem("Cut");
+		cutOption.setActionCommand("CUT");
+		cutOption.addActionListener(this);
+		cutOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
+		editMenu.add(cutOption);
+
+		JMenuItem copyOption = new JMenuItem("Copy");
+		copyOption.setActionCommand("COPY");
+		copyOption.addActionListener(this);
+		copyOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+		editMenu.add(copyOption);
+
+		JMenuItem pasteOption = new JMenuItem("Paste");
+		pasteOption.setActionCommand("PASTE");
+		pasteOption.addActionListener(this);
+		pasteOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
+		editMenu.add(pasteOption);
+
+		menuBar.add(editMenu);
+
+		JMenu helpMenu = new JMenu("Help");
+
+		JMenuItem helpOption = new JMenuItem("Help Contents  ");
+		helpOption.setActionCommand("HELP");
+		helpOption.addActionListener(this);
+		helpOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
+		helpMenu.add(helpOption);
+		helpMenu.addSeparator();
+
+		JMenuItem aboutOption = new JMenuItem("About");
+		aboutOption.setActionCommand("ABOUT");
+		aboutOption.addActionListener(this);
+		helpMenu.add(aboutOption);
+
+		menuBar.add(helpMenu);
+
+		this.add(menuBar, BorderLayout.NORTH);
+
+	}
+
+	/**
+	 * Sets up the tool bar. The tool bar contains the buttons Add Class and Delete
+	 */
+	private void setUpToolBar()
+	{
+		toolBar = new JToolBar();
+
+		Icon addClassIcon = new ImageIcon("icons/class.gif");
+		Icon deleteIcon = new ImageIcon("icons/delete.gif");
+
+		addClassButton = new JButton("Add Class", addClassIcon);
+		addClassButton.setActionCommand("ADD");
+		addClassButton.addActionListener(this);
+		addClassButton.setFocusable(false);
+		addClassButton.setFocusPainted(false);
+		toolBar.add(addClassButton);
+		addNewClassModeEnabled = false;
+
+		deleteButton = new JButton("Delete", deleteIcon);
+		deleteButton.setActionCommand("DELETE");
+		deleteButton.addActionListener(this);
+		deleteButton.setEnabled(false);
+		toolBar.add(deleteButton);
+
+		this.add(toolBar, BorderLayout.SOUTH);
+	}
+
+	/**
+	 * Sets up a scroll pane.
+	 */
+	private void setUpTabbedPane()
+	{
+		tabbedPane = new JTabbedPane();
+		tabbedPane.setFocusable(false);
+		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		this.add(tabbedPane, BorderLayout.CENTER);
+	}
+
+	private ClassDiagram loadClassFromFile()
+	{
+		JFileChooser fileLoadChooser = new JFileChooser();
+		fileLoadChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fileLoadChooser.setMultiSelectionEnabled(false);
+		fileLoadChooser.addChoosableFileFilter(new FileExtensionFilter());
+		int userChoice = fileLoadChooser.showOpenDialog(this);
+
+		ClassDiagram loadedDiagram = null;
+
+		if (userChoice == JFileChooser.APPROVE_OPTION)
+		{
+			File f = fileLoadChooser.getSelectedFile();
+			try
+			{
+				FileReader fileInStream;
+				BufferedReader buffInStream;
+
+				// code to save to file goes here
+				XStream xmlStream = new XStream();
+
+				fileInStream = new FileReader(f);
+				buffInStream = new BufferedReader(fileInStream);
+
+				loadedDiagram = (ClassDiagram) xmlStream.fromXML(buffInStream);
+
+				buffInStream.close();
+			}
+			catch (IOException e)
+			{
+
+			}
+			finally
+			{
+
+			}
+		}
+
+		return loadedDiagram;
+	}
+
+	/**
+	 * Initializes a new ClassDiagram.
+	 */
+	private void createNewClassDiagram()
+	{
+		JScrollPane scrollPane = new JScrollPane();
+		ClassDiagram initialDiagram = new ClassDiagram(this, scrollPane);
+		classDiagrams.add(initialDiagram);
+		tabbedPane.add(scrollPane);
+		if (!classDiagrams.isEmpty())
+		{
+			tabbedPane.setSelectedIndex(classDiagrams.size() - 1);
+		}
+		tabbedPane.setTabComponentAt(tabbedPane.getSelectedIndex(), new TabComponent(this, tabbedPane,
+				"Unsaved Diagram"));
+		addClassButton.setEnabled(true);
+		initialDiagram.requestFocusOnView();
+	}
+
+	private void setUpHelpPanel()
+	{
+		helpPanel = new HelpPanel();
+		helpPanel.setVisible(false);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+		if (arg0.getActionCommand() == "ADD")
+		{
+			if (!addNewClassModeEnabled)
+			{
+				// Add-Class mode was not enabled, so enable it
+				enableAddNewClassMode();
+			}
+			else
+			{
+				// Add-Class mode was already selected, this click disables it
+				disableAddNewClassMode();
+			}
+		}
+		else if (arg0.getActionCommand() == "DELETE")
+		{
+			ClassDiagram currentDiagram = getCurrentDiagram();
+			currentDiagram.deleteSelectedObject();
+		}
+		else if (arg0.getActionCommand() == "NEW")
+		{
+			createNewClassDiagram();
+			this.validate();
+		}
+		else if (arg0.getActionCommand() == "LOAD")
+		{
+			disableAddNewClassMode();
+			loadClassFromFile();
+			this.validate();
+		}
+		else if (arg0.getActionCommand() == "SAVE")
+		{
+			ClassDiagram currentDiagram = getCurrentDiagram();
+			currentDiagram.saveToFile(false);
+		}
+		else if (arg0.getActionCommand() == "SAVEAS")
+		{
+			ClassDiagram currentDiagram = getCurrentDiagram();
+			currentDiagram.saveToFile(true);
+		}
+		else if (arg0.getActionCommand() == "CLOSE")
+		{
+			closeCurrentTab();
+		}
+		else if (arg0.getActionCommand() == "EXIT")
+		{
+			closeEditor();
+		}
+		else if (arg0.getActionCommand() == "CUT")
+		{
+			ClassDiagram currentDiagram = getCurrentDiagram();
+			currentDiagram.cutNode();
+		}
+		else if (arg0.getActionCommand() == "COPY")
+		{
+			ClassDiagram currentDiagram = getCurrentDiagram();
+			currentDiagram.copyNode();
+		}
+		else if (arg0.getActionCommand() == "PASTE")
+		{
+			ClassDiagram currentDiagram = getCurrentDiagram();
+			currentDiagram.pasteNode();
+		}
+		else if (arg0.getActionCommand() == "HELP")
+		{
+			helpPanel.setToContentsTab();
+			helpPanel.setVisible(true);
+		}
+		else if (arg0.getActionCommand() == "ABOUT")
+		{
+			helpPanel.setToAboutTab();
+			helpPanel.setVisible(true);
+		}
+	}
+
+	private void closeEditor()
+	{
+		int numOpenTabs = tabbedPane.getTabCount();
+		for (int i = 0; i < numOpenTabs; ++i)
+		{
+			boolean closedTab = closeCurrentTab();
+			if (!closedTab)
+			{
+				break;
+			}
+		}
+
+		if (classDiagrams.isEmpty())
+		{
+			// we removed all diagrams without the user canceling,
+			// so go ahead and close the editor
+			this.dispose();
+		}
+	}
+
+	public boolean closeCurrentTab()
+	{
+		ClassDiagram currentDiagram = getCurrentDiagram();
+		int userOption = JOptionPane.NO_OPTION;
+		if (currentDiagram.isUnsaved())
+		{
+			userOption = JOptionPane.showConfirmDialog(this,
+					"There are unsaved changes to the current diagram. \nDo you want to save before closing?");
+		}
+
+		if (userOption == JOptionPane.YES_OPTION)
+		{
+			currentDiagram.saveToFile(false);
+		}
+
+		if (userOption != JOptionPane.CANCEL_OPTION && userOption != JOptionPane.CLOSED_OPTION)
+		{
+			tabbedPane.remove(tabbedPane.getSelectedIndex());
+			classDiagrams.remove(currentDiagram);
+			if (classDiagrams.isEmpty())
+			{
+				addClassButton.setEnabled(false);
+			}
+			return (true);
+		}
+		return (false);
+	}
+
+	private ClassDiagram getCurrentDiagram()
+	{
+		int currentIndex = tabbedPane.getSelectedIndex();
+		ClassDiagram openDiagram = classDiagrams.get(currentIndex);
+		return (openDiagram);
+	}
+
+	public ClassNode getCopyNode()
+	{
+		return (copyNode);
+	}
+
+	public void setCopyNode(ClassNode coppiedNode)
+	{
+		copyNode = new ClassNode(coppiedNode);
+	}
+
+	/**
+	 * Entry point for the program.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		new UMLEditor();
+	}
+
+	private class WindowCloseListener extends WindowAdapter
+	{
+		@Override
+		public void windowClosing(WindowEvent e)
+		{
+			closeEditor();
+		}
+	}
+}
