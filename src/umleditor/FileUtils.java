d1460cec4b8e67905fd6dd6fcdc8e15e1ab9f81b
Ky Waegel <kwaegel@gmail.com>
2011-04-02 18:21:35 -0400

    Added use of xml attributes for easier reading.

--- src/umleditor/FileUtils.java
+++ src/umleditor/FileUtils.java
@@ -9,6 +9,10 @@ import com.thoughtworks.xstream.XStream;
 public class FileUtils
 {
 
+	/**
+	 * 
+	 * @return an {@link XStream} object to be used of reading and writing.
+	 */
 	public static XStream getXmlReaderWriter()
 	{
 		XStream xmlStream = new XStream();
@@ -20,22 +24,25 @@ public class FileUtils
 		// Alias names for class nodes
 		xmlStream.alias("class", ClassNode.class);
 		xmlStream.aliasField("name", ClassNode.class, "className");
+		xmlStream.useAttributeFor(ClassNode.class, "className");
 		xmlStream.aliasField("attributes", ClassNode.class, "listOfAttributes");
 		xmlStream.aliasField("methods", ClassNode.class, "listOfMethods");
 		xmlStream.aliasField("relationships", ClassNode.class, "m_relationships");
 		xmlStream.aliasField("location", ClassNode.class, "m_location");
 
 		// Alias for relationship data
-		xmlStream.alias("relationshipData", RelationshipModel.class);
+		xmlStream.alias("relationship", RelationshipModel.class);
 		xmlStream.aliasField("type", RelationshipModel.class, "m_type");
-		xmlStream.aliasField("pointList", RelationshipModel.class, "m_points");
-		xmlStream.aliasField("type", RelationshipModel.class, "m_type");
+		xmlStream.useAttributeFor(RelationshipModel.class, "m_type");
+		xmlStream.aliasField("points", RelationshipModel.class, "m_points");
 		xmlStream.aliasField("firstClassNode", RelationshipModel.class, "m_firstNode");
 		xmlStream.aliasField("secondClassNode", RelationshipModel.class, "m_secondNode");
 		xmlStream.aliasField("firstNodeOffset", RelationshipModel.class, "m_firstNodeOffset");
 		xmlStream.aliasField("secondNodeOffset", RelationshipModel.class, "m_secondNodeOffset");
 
 		xmlStream.alias("point", java.awt.Point.class);
+		xmlStream.useAttributeFor(java.awt.Point.class, "x");
+		xmlStream.useAttributeFor(java.awt.Point.class, "y");
 
 		// Alias field names
 
