bc1ed46d43f21a1a670486d093cedb2036a1abb5
lmcrous1 <lmcrous1@localhost>
2011-04-02 19:22:17 -0400

    changed file chooser properties

--- src/umleditor/FileExtensionFilter.java
+++ src/umleditor/FileExtensionFilter.java
@@ -16,6 +16,10 @@ class FileExtensionFilter extends javax.swing.filechooser.FileFilter
 	@Override
 	public boolean accept(File file)
 	{
+		if (file.isDirectory ())
+		{
+			return (true);
+		}
 		String filename = file.getName();
 		return filename.endsWith(ACCEPTED_FILE_EXTENSION);
 	}
