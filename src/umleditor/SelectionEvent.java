2ad39e6f4f8708f6624360da3d9f37c48118708e
MaxMSeibert <MaxMSeibert@localhost>
2011-04-20 22:36:54 -0400

    Added additional comments

--- src/umleditor/SelectionEvent.java
+++ src/umleditor/SelectionEvent.java
@@ -2,6 +2,10 @@ package umleditor;
 
 import javax.swing.event.ChangeEvent;
 
+/**
+ * Used with EventPublisher. Informs the Event Publisher if an object has been 
+ * selected.
+ */
 public class SelectionEvent extends ChangeEvent
 {
 	private static final long serialVersionUID = -5342354909362780034L;
@@ -19,17 +23,26 @@ public class SelectionEvent extends ChangeEvent
 		m_singleSelectionRequested = singleSelection;
 	}
 
+	/**
+	 * Returns the selected ISelectable object(s)
+	 */
 	@Override
 	public ISelectable getSource()
 	{
 		return (ISelectable) this.source;
 	}
 
+	/**
+	 * Returns true if only one object is selected
+	 */
 	public boolean isSingleSelectionRequested()
 	{
 		return m_singleSelectionRequested;
 	}
 
+	/**
+	 * Returns a string stating the selected ISelectable object(s)
+	 */
 	@Override
 	public String toString()
 	{
